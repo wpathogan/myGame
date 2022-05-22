f74fd8a0e757a1db8753e6a9802e11afac71bec7
pat <wpathogan91@gmail.com>
2022-05-22 19:02:58 -0400

    love tutorial-add movement to basic character.

---
+++ main.lua
@@ -0,0 +1,25 @@
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+    player.speed = 3
+end 
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+end
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 100)
+end
